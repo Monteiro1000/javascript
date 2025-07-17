ad00dbc00ace474adf05921526754b9b7e6b5e67
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2025-07-17 17:29:06 -0300

    Iniciando exercício com o uso de javascript

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,8 @@
+function carregar () {
+    var msg = window.document.querySelector('div#msg')
+    var img = window.document.querySelector('div#imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+}
