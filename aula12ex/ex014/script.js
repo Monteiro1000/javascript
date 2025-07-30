69ef1403e17d75a5ee11a9df7b792dfa5c1917bc
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2025-07-30 10:01:32 -0300

    Corrigindo alguns erros no código

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,15 +1,16 @@
 function carregar () {
     var msg = window.document.querySelector('div#msg')
-    var img = window.document.querySelector('div#imagem')
+    var img = window.document.querySelector('img#imagem')
     var data = new Date()
     var hora = data.getHours()
+    
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
-        img.src = 'fotomanha.jpg'
+        img.src = 'imagens/fotomanha.jpg'
     } else if (hora >= 12 && hora < 18) {
-        img.src = 'fototarde.jpg'
+        img.src = 'imagens/fototarde.jpg'
     } else {
-        img.src = 'fotonoite.jpg'
+        img.src = 'imagens/fotonoite.jpg'
     }
 
     } 
