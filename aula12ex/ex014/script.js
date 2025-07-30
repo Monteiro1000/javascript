c89c9536ab85320fe44417504ca241a52d79b02d
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2025-07-30 15:57:31 -0300

    Finalizando o primeiro exercício de JavaScript

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -7,10 +7,17 @@ function carregar () {
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         img.src = 'imagens/fotomanha.jpg'
+
+        document.body.style.background = '#0487D9'
+
     } else if (hora >= 12 && hora < 18) {
         img.src = 'imagens/fototarde.jpg'
+        document.body.style.background = '#F25C05'
+
     } else {
         img.src = 'imagens/fotonoite.jpg'
+
+        document.body.style.background = '#494859'
     }
 
     } 
