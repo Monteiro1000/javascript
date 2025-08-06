b5403ad08739daf169f8c0d5acca26b313dbd0e9
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2025-08-06 20:59:04 -0300

    Resolvendo alguns erros e avançando na atividade

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,13 +1,20 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.querySelector('div#iano')
+    var fano = document.querySelector('input#iano')
     var res = document.querySelector('div#res')
-
-    if(fano.ariaValueMax.length == 0 || Number(fano.value) > ano) {
-        alert('[Erro] Verifique os dados e tente novamente!')
+    
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[Erro] Verifique os dados e tente novamente!')
     } else {
-        var fsex = document.getElementsByTagName('radsex')
+        var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
+        var gênero = ''
+        if (fsex[0].checked) {
+         gênero = 'Homem'
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+        }
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
     }
 }
