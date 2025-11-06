0a2fd5e3edc38ef2b366c089000805efa085f306
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2025-11-06 15:35:21 -0300

    Terminando de estudar a estrutura utilizada para fazer uma tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -7,6 +7,13 @@ function tabuada() {
     }else{
         let n = Number(num.value)
         let c = 1
-        tab.innerHTML = ''
+        tab.innerHTML = '' 
+        while(c <= 10) {
+            let item = document.createElement('option')
+            item.text = ` ${n} X ${c} = ${n*c}`
+            item.value = `tab ${c}`
+            tab.appendChild(item)
+            c++
+        }
     }
 }
