d840df6c47ae3800b6405ca30b6c0cd34eb2049a
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2025-11-13 21:49:13 -0300

    Aprendendo mais sobre vetores

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,5 +1,11 @@
 let num = [5, 8, 2, 9, 3]
 
-console.log(`O vetor ten ${num.length} posições`)
+console.log(`O vetor tem ${num.length} posições`)
 
-
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
+if (pos == -1){
+    console.log(' O valor não foi encontrado')
+}else { 
+    console.log(`O valor 8 está na posição ${pos}`)
+}
