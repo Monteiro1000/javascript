106cabb97d95eff2fc1668f4168badc99ba8db6f
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2025-11-24 19:46:03 -0300

    Começando um desafio de javascript

--- desafios/D18/script.js
+++ desafios/D18/script.js
@@ -1,3 +1,8 @@
-function adicionar(valor) {
-    
+function adicionar() {
+    let num = document.getElementById('inum')
+    let tab = document.getElementById('ivalor')
+
+    if(num == '') {
+        alert("Valor inválido ou já definido")
+    }
 }
