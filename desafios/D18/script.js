ee72edcfdd6c97e2dda2b818fee83483cc556e8b
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2025-12-01 21:45:27 -0300

    Dando mais um passo no desafio

--- desafios/D18/script.js
+++ desafios/D18/script.js
@@ -2,7 +2,7 @@ function adicionar() {
     let num = document.getElementById('inum')
     let tab = document.getElementById('ivalor')
 
-    if(num == '') {
+    if(num.value >= 100 || num.value.length == 0) {
         alert("Valor inválido ou já definido")
     }
 }
